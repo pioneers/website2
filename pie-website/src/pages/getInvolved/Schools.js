dfe06a14d528de776cfd14532379a29e534d4e61
Jonathan Atkins <jonathan.atkins@berkeley.edu>
2021-08-28 11:55:42 -0700

    fixed?

--- pie-website/src/pages/getInvolved/Schools.js
+++ pie-website/src/pages/getInvolved/Schools.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 import CustomizedContainer from "../../components/CustomizedContainer"
-import SEO from "../../components/Seo"
 import Layout from "../../components/Layout"
+import SEO from "../../components/SEO"
 import theme from "../../assets/themes/theme"
 import styled from "styled-components"
 const Schools = () => {
