cf6a36290e3acb9228474506a9bf039df7b5fb3f
Jonathan Atkins <jonathan.atkins@berkeley.edu>
2023-01-29 21:50:53 -0800

    added index

--- pie_website/src/pages/index.js
+++ pie_website/src/pages/index.js
@@ -1,176 +1,247 @@
-import * as React from "react"
+import React from "react";
+import Layout from "../components/Layout";
+import styled from "styled-components";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+import theme from "../styles/theme";
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+import pic from "../images/explaining.jpg";
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
+import Header from "../components/Header";
+import Btn from "../components/Btn";
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+import { FaHandsHelping } from "@react-icons/all-files/fa/FaHandsHelping";
+import { BiGroup } from "@react-icons/all-files/bi/BiGroup";
+import { VscOpenPreview } from "@react-icons/all-files/vsc/VscOpenPreview";
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
+import Block from "../components/Block";
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import Container from "react-bootstrap/esm/Container";
 
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+import CLink from "../components/CLink";
 
-const IndexPage = () => {
+import MailchimpFormContainer from "../components/Mailchimp";
+
+import { AnchorLink } from "gatsby-plugin-anchor-links";
+
+import { SEO } from "../components/SEO";
+
+export default function Home(props) {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
+    <Layout>
+      <SEO />
+      <Hero>
+        <div className="hero-image">
+          <div>
+            <h1>Pioneers in Engineering</h1>
+            <h3>Bringing STEM education to the Bay Area</h3>
+          </div>
+        </div>
+      </Hero>
+
+      <Block>
+        <Container fluid>
+          <h4 style={{ marginBottom: "1.5em", color: theme.colors.grey400 }}>
+            WHO WE ARE
+          </h4>
+          <h2 style={{ maxWidth: "700px", marginBottom: "1rem" }}>
+            Promoting STEM education to the Bay Area because we strongly believe
+            that no student should be denied a quality educational experience
+          </h2>
+          <Btn type="hipster" link="/GetInvolved#timeline">
+            Events
+          </Btn>
+          <Btn color="gold" link="/getInvolved/Forms">
+            Join Pie
+          </Btn>
+          <br />
+        </Container>
+
+        <WhoWeServe fluid>
+          <Row>
+            <Col xs={12} sm={4} md={4} lg={4}>
+              <div>
+                <h3>350+</h3>
+                <p>Berkeley students have contributed to our competition </p>
+              </div>
+            </Col>
+            <Col xs={12} sm={4} md={4} lg={4}>
+              <div>
+                <h3>700+</h3>
+                <p>high school competitors over the years</p>
+              </div>
+            </Col>
+            <Col xs={12} sm={4} md={4} lg={4}>
+              <div>
+                <h3>30+</h3>
+                <p>
+                  Bay Area schools that have participated in our competition
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </WhoWeServe>
+      </Block>
+
+      <Block color="white">
+        <JoinUsHeader fluid>
+          <h4 style={{ marginBottom: "1.5em", color: theme.colors.grey400 }}>
+            GET INVOLVED
+          </h4>
+          <Header type={"blue-header"}>Be a part in our competition</Header>
+          <p style={{ marginTop: "1.5rem" }}>
+            Excited to participate? Sign up as a school, a mentor, or a general
+            club member!
+          </p>
+        </JoinUsHeader>
+        <JoinUs fluid>
+          <Row>
+            <Col sm={12} md={4} lg={4}>
+              <div>
+                <FaHandsHelping />
+                <h4>Become a mentor</h4>
+                <p>
+                  Interested in participating in PiE's mission by becoming a
+                  mentor and role model for a team of high school students? PiE
+                  RC DeCal, offered every spring, offers just that!
+                </p>
+                <CLink to="/getInvolved/Decal">Learn More &rsaquo;</CLink>
+              </div>
+            </Col>
+            <Col sm={12} md={4} lg={4}>
+              <div>
+                <BiGroup />
+                <h4>Join our team</h4>
+                <p>
+                  Join our team and be part of providing STEM education to the
+                  Bay Area.
+                </p>
+                <CLink to="/getInvolved/Forms">Learn More &rsaquo;</CLink>
+              </div>
+            </Col>
+            <Col sm={12} md={4} lg={4}>
+              <div>
+                <VscOpenPreview />
+                <h4>School Sign Up</h4>
+                <p>
+                  Want to participate in our 8 week long competition? Learn more
+                  and sign up today.
+                </p>
+                <CLink to="https://google.com">Learn More &rsaquo;</CLink>
+              </div>
+            </Col>
+          </Row>
+        </JoinUs>
+      </Block>
+      <Block color={theme.colors.grey100}>
+        <div id="mailinglist"></div>
+        <MailchimpFormContainer />
+      </Block>
+    </Layout>
+  );
 }
 
-export default IndexPage
+const Hero = styled.div`
+  z-index: 0;
+  .hero-image {
+    background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.5)),
+      url(${pic}) center/cover fixed no-repeat;
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: ${theme.colors.white};
+    top: 0;
+    z-index: -1;
+    position: relative;
 
-export const Head = () => <title>Home Page</title>
+    padding: 1rem;
+    h1 {
+      font-size: 5rem;
+    }
+  }
+`;
+
+const JoinUsHeader = styled(Container)`
+  a {
+    font-weight: 600;
+    color: ${theme.colors.blue500};
+  }
+  a:hover {
+    color: ${theme.colors.black};
+  }
+`;
+
+const Students = styled(Container)`
+  h2 {
+    margin-bottom: 2rem;
+  }
+  p {
+    margin-bottom: 1.5rem;
+  }
+  p > a {
+    font-weight: 600;
+    color: ${theme.colors.blue500};
+  }
+  p > a:hover {
+    color: ${theme.colors.black};
+  }
+`;
+
+const JoinUs = styled(Container)`
+  h4 {
+    border-left: 1px ${theme.colors.blue600} solid;
+    /* margin-bottom: 0; */
+    line-height: 1.2rem;
+    padding-left: 0.75rem;
+    color: ${theme.colors.blue600};
+    margin: 0.75rem 0;
+  }
+  svg {
+    padding: 0 0 0 0.75rem;
+    font-size: 3.5rem;
+  }
+  p {
+    padding-left: 0.75rem;
+    color: ${theme.colors.grey600};
+    margin-bottom: 0.5rem;
+    font-weight: 400;
+  }
+  a {
+    padding-left: 0.75rem;
+    color: ${theme.colors.gold600};
+  }
+  a:hover {
+    color: ${theme.colors.black};
+  }
+  .row > div {
+    margin: 1.25rem 0;
+  }
+`;
+
+const WhoWeServe = styled(Container)`
+  h3 {
+    border-left: 1px ${theme.colors.blue600} solid;
+    line-height: 1.2rem;
+    padding-left: 0.75rem;
+    color: ${theme.colors.blue600};
+  }
+  p {
+    padding-left: 0.75rem;
+  }
+  .row > div {
+    margin: 1.25rem 0;
+  }
+  @media screen and (max-width: 575px) {
+    h3 {
+      font-size: 2rem;
+      line-height: 2rem;
+    }
+    p {
+      font-size: 1.5rem;
+    }
+  }
+`;
