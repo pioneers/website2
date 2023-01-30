01d3a134cda16941a5540a0352b8d8b1f4ff68b7
Jonathan Atkins <jonathan.atkins@berkeley.edu>
2023-01-29 21:53:36 -0800

    added 404

--- pie_website/src/pages/404.js
+++ pie_website/src/pages/404.js
@@ -1,49 +1,61 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import Container from "react-bootstrap/esm/Container";
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import styled from "styled-components";
+import Btn from "../components/Btn";
+import GlobalStyles from "../styles/globalStyles";
+import WellDone from "../svg/undraw_well_done_i2wr.svg";
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-const NotFoundPage = () => {
+const Error = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <React.Fragment>
+      <GlobalStyles />
+      <Wrapper>
+        <Container fluid>
+          <object
+            style={{ margin: ".2rem" }}
+            data={WellDone}
+            type="image/svg+xml"
+            alt="Some Balloons"
+            aria-label="Some Balloons"
+          ></object>
+          <div>
+            <h1>404</h1>
+            <h3>Page Not Found</h3>
+            <Btn link="/">Home &rsaquo;</Btn>
+          </div>
+        </Container>
+      </Wrapper>
+    </React.Fragment>
+  );
+};
 
-export default NotFoundPage
+const Wrapper = styled.div`
+  object {
+    z-index: -1;
+  }
+  & > div {
+    display: flex;
 
-export const Head = () => <title>Not found</title>
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  h1 {
+    font-size: 5rem;
+  }
+  h3 {
+    font-size: 3rem;
+  }
+
+  @media only screen and (max-width: 964px) {
+    object {
+      display: none;
+    }
+  }
+`;
+
+export default Error;
