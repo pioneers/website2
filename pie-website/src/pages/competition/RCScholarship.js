69acdbb7b29c7d708a758d912d11bb833af329d8
Matthew Chang <mattchang@pioneers.berkeley.edu>
2023-10-12 05:23:34 +0000

    [WIP] Update Scholarship schedule and links for 2024
    * Updated schedule for 2023-2024 year
    * Update headers for new workshop slides and application pdf
    
    TODO:
    * Update workshop slides
    * Update application pdf

--- pie-website/src/pages/competition/RCScholarship.js
+++ pie-website/src/pages/competition/RCScholarship.js
@@ -88,14 +88,14 @@ const RCScholarship = () => {
           <h4 style={{ marginBottom: "1.5em", color: theme.colors.grey400 }}>
             THE TIMELINE
           </h4>
-          <Header type="gold">2023 Scholarship Schedule</Header>
+          <Header type="gold">2024 Scholarship Schedule</Header>
           <ScheduleWrapper fluid align="center" padding="3rem 1.25rem">
             <Row className="inner-container">
               <Col sm={12} md={6} lg={3}>
                 <div className="card">
                   <div>
                     <VscDebugStart />
-                    <h3>Application opens Saturday 3/4/2023</h3>
+                    <h3>Application opens Saturday 10/14/2023</h3>
                   </div>
                 </div>
               </Col>
@@ -103,7 +103,7 @@ const RCScholarship = () => {
                 <div className="card">
                   <div>
                     <AiOutlineFileDone />
-                    <h3>EXTENDED!! Application due Friday 4/21/2023</h3>
+                    <h3>Application due Tuesday 3/12/2024</h3>
                   </div>
                 </div>
               </Col>
@@ -111,7 +111,7 @@ const RCScholarship = () => {
                 <div className="card">
                   <div>
                     <FaUserTie />
-                    <h3>Interviews with finalists - Saturday 4/29/2023</h3>
+                    <h3>Interviews with finalists - Week of 4/1/2024 thru 4/5/2024</h3>
                   </div>
                 </div>
               </Col>
@@ -119,7 +119,7 @@ const RCScholarship = () => {
                 <div className="card">
                   <div>
                     <GiPodiumWinner />
-                    <h3>Recipient announced - Sunday 4/30/2023</h3>
+                    <h3>Recipient announced - Sunday 4/28/2024</h3>
                   </div>
                 </div>
               </Col>
@@ -159,7 +159,7 @@ const RCScholarship = () => {
                 <Col sm={12} md={12} lg={12}>
                   <div>
                     <BiVideoRecording />
-                    <h4>2023 Workshop</h4>
+                    <h4>2024 Workshop</h4>
                     <p>
                       <Link to="https://pioneers.berkeley.edu/assets/scholarship/2023_pie_scholarship_workshop_presentation.pdf">
                         {" "}
@@ -171,7 +171,7 @@ const RCScholarship = () => {
                 <Col sm={12} md={12} lg={12}>
                   <div>
                     <AiOutlineUnorderedList />
-                    <h4>2023 Scholarship Application Summary</h4>
+                    <h4>2024 Scholarship Application Summary</h4>
                     <p>
                       <Link to="https://pioneers.berkeley.edu/assets/scholarship/2023_pie_scholarship_application.pdf">
                         {" "}
